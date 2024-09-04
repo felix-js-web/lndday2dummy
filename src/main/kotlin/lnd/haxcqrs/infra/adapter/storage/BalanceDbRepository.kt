34c686f6546d660e0ed7f10945e366db4959736d
FelixA <felix.java.web@gmail.com>
2024-09-05 09:04:54 +1000

    JIRA-4444 added basic get for balances with wallet id working now

--- src/main/kotlin/lnd/haxcqrs/infra/adapter/storage/BalanceDbRepository.kt
+++ src/main/kotlin/lnd/haxcqrs/infra/adapter/storage/BalanceDbRepository.kt
@@ -1,17 +1,40 @@
 package lnd.haxcqrs.infra.adapter.storage
 
+import lnd.haxcqrs.demo.User
+import lnd.haxcqrs.demo.UserRepository
 import lnd.haxcqrs.domain.model.Balance
 import lnd.haxcqrs.domain.repository.BalanceRepository
 import org.springframework.context.annotation.Profile
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
 
 @Repository
 @Profile("default")
-class BalanceDbRepository : BalanceRepository {
+class BalanceDbRepository(private val jdbcTemplate: JdbcTemplate) : BalanceRepository {
 
+    private val rowMapper = RowMapper<Balance> { rs, _ ->
+        Balance(
+                id = rs.getString("id") ?: "",
+                transactionId = rs.getString("transactionId") ?: "",
+                amount = rs.getLong("amount"),
+                version = rs.getInt("version"),
+                balance = rs.getLong("balance"),
+                createdDate = rs.getTimestamp("createdDate").toLocalDateTime()
+        )
+    }
     override fun returnBalanceById(balanceId: String): Balance? {
 //        return null
-        return Balance(balanceId, "1", 100, 1, 100)
+        return try {
+            jdbcTemplate.queryForObject(
+                    "SELECT * FROM balance WHERE id = ? ORDER BY version DESC LIMIT 1",
+                    rowMapper,
+                    balanceId
+            )
+        } catch (e: EmptyResultDataAccessException) {
+            null
+        }
     }
 
     override fun saveBalance(balance: Balance): Balance {
@@ -19,3 +42,17 @@ class BalanceDbRepository : BalanceRepository {
     }
 
 }
+
+//    private val rowMapper = RowMapper<User> { rs, _ ->
+//        User(
+//                id = rs.getLong("id"),
+//                name = rs.getString("name")
+//        )
+//    }
+//
+//    override fun findAll(): List<User> =
+//            jdbcTemplate.query("SELECT * FROM users", rowMapper)
+//
+//    override fun save(user: User): Int =
+//            jdbcTemplate.update("INSERT INTO users (name) VALUES (?)", user.name)
+//}
